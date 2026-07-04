955e8ff6163c23b3fd4a213e6c832b65717a5650
Jono <hazies@gmail.com>
2026-07-04 08:30:30 -0700

    Update build script.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ echo "If this build fails with a free disk space error, run:"
 echo "  ostree --repo=repo config set core.min-free-space-percent 0"
 echo "    and then try again."
 echo ""
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak run org.flatpak.Builder --force-clean --sandbox --user --install-deps-from=flathub --disable-rofiles-fuse --ccache --repo=repo flatpak_build io.github.e_j_w.ChartOfNuclides-master.yml
 flatpak build-bundle repo ChartOfNuclides.flatpak io.github.e_j_w.ChartOfNuclides
 if [ -f ChartOfNuclides.flatpak ]; then
